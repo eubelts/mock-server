6346cdc130a6f6aeb48c85215dd00bfcfa6180ce
eubelts <eunicesausbeltran@yahoo.com>
2018-05-02 17:04:05 +0800

    Added assets precompile for jsoneditor-icons

--- app/controllers/mocks_controller.rb
+++ app/controllers/mocks_controller.rb
@@ -4,12 +4,14 @@ class MocksController < ApplicationController
     url = request.path
     @mock = Mock.where(route_path: params[:path]).last
 # binding.pry
+    if @mock
     if "/#{@mock.route_path}" == url
         @respon = @mock.response
         render json: @respon
     else
       #render error
     end
+  end
   end
 
 
