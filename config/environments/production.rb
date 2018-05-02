ed2e8b5caf3079a7e3174c0489b32137a5a21442
eubelts <eunicesausbeltran@yahoo.com>
2018-05-02 17:08:21 +0800

    added assets precompile in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  config.assets.precompile += %w( jsoneditor-icons.svg )
+  config.assets.precompile += %w[*.png *.jpg *gif]
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
